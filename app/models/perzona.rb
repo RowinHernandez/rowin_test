78c593404531fcb8e8dd87f4ad497aca584ef177
Sduy <rowincho@live.com>
2013-09-09 11:42:12 -0430

    agregando validaciones

--- app/models/perzona.rb
+++ app/models/perzona.rb
@@ -3,4 +3,7 @@ class Perzona < ActiveRecord::Base
   validates_numericality_of :cedula 
   validates_presence_of :nombre, :cedula, :apellido, :direccion 
   validates_uniqueness_of :cedula
+  validates_length_of :cedula, :minimum => 6, :maximum => 8, :allow_blank => true
+  validates :nombre, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 end 
